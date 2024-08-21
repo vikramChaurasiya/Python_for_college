fe95268e2f0bc4824688c4edc653cc374dcff367
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-21 13:13:23 +0530

    Practice question

---
+++ 01_String/stringpracticeqes.py
@@ -0,0 +1,11 @@
+
+# Q1. WAP to input user's first name & print it s length.
+
+str = str(input("Enter your First name:-"))
+# print("Length of your name is ",len( str))
+
+
+#  Q2. WAP to find occurrence of '$' in a String.
+
+str = "Hi, $I amthe $ Symol $99.99"
+print (str.count("$"))
