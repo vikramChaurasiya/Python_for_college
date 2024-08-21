e156be4da13eabd4c66252eb8f15d07168be75e4
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-21 12:07:00 +0530

    String basic

---
+++ 01_String/basicstr.py
@@ -0,0 +1,32 @@
+# how store string
+
+str = "vikram "
+str1= 'hello i am vikram'
+str2= """Hello i am vikram"""
+print(str)
+print(str1)
+print(str2)
+
+# concatenation 
+
+str ="I say"
+str2 = "Hello"
+print(str+" "+str2)
+
+# length of string 
+
+print(len(str2))
+
+# How access Sting index.
+# in python always index start with zero(0).
+
+str ="Apna_college"
+print(str[0])
+
+#  Slicing:-> it is a part of astring for example. 
+# in slicing end index is not include in string.
+
+str = "Apna_college"
+print(str[0:3])
+
+
