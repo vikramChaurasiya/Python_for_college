225d6f03d05fa095b2b9aa3bec358b8668abc100
vikramChaurasiya <bhartivikram633@gmail.com>
2024-10-26 17:30:38 +0530

    Loop

---
+++ 05_Loop/whileLoopPr.py
@@ -0,0 +1,18 @@
+#qs1.
+# i =1 
+# while i<=100 :
+#     print(i)
+#     i=i+1
+
+#qs2.
+# i =100
+# while i>= 1:
+#     print(i)
+#     i=i-1
+
+#qs3
+
+i=1;
+while i<=10 :
+    print(i*2)
+    i=i+1
